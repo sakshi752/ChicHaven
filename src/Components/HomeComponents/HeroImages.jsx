8e7522a65369f23d8d694a9d218163ce6642d861
unknown <saks0705@gmail.com>
2024-03-31 23:34:37 +0530

    files updated

--- src/Components/HomeComponents/HeroImages.jsx
+++ src/Components/HomeComponents/HeroImages.jsx
@@ -2,8 +2,8 @@ import React from 'react'
 
 const HeroImages = ({img,imgTitle}) => {
   return (
-    <div className="relative max-h-60"> 
-        <img src={img} alt={imgTitle} className='h-full w-full object-cover' />
+    <div className="relative max-h-52 cursor-pointer"> 
+        <img src={img} alt={imgTitle} className='h-full w-full object-cover ' />
         <div className="absolute inset-0 bg-black opacity-50"></div>
         <div className="absolute inset-0 flex justify-center  items-center">
           <span className="text-white text-lg font-bold tracking-wide">{imgTitle}</span>
--- src/Components/HomeComponents/Home.jsx
+++ src/Components/HomeComponents/Home.jsx
@@ -3,8 +3,10 @@ import HeroSection from './HeroSection'
 
 const Home = () => {
   return (
-    <div className='mt-5 md:mt-10'>
+    <div className='mt-5 md:mt-6'>
       <HeroSection/>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Vero quos aperiam, reprehenderit quibusdam earum nam. Explicabo quidem expedita magni debitis.
+      
     </div>
   )
 }
