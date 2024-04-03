8583058f127fd9acc3f5117b40d025d7d0e54889
unknown <saks0705@gmail.com>
2024-04-03 22:01:00 +0530

    files updated

--- src/Components/HomeComponents/ProudProducts.jsx
+++ src/Components/HomeComponents/ProudProducts.jsx
@@ -1,14 +1,34 @@
-import React from 'react'
+import React from 'react';
+import { useSelector } from 'react-redux';
+import CardComponent from '../CardComponent';
 
 const ProudProducts = () => {
+  const data = useSelector(state => state.categories.data);
+
+  const getRandomIndices = (max, count) => {
+    const indices = [];
+    while (indices.length < count) {
+      const randIndex = Math.floor(Math.random() * max)
+      if (!indices.includes(randIndex)) {
+        indices.push(randIndex);
+      }
+    }
+    return indices; // Return the array of indices
+  }
+
+  const randomIndices = getRandomIndices(data.length, 4);
+
+  const randomProduct = randomIndices.map((index) => data[index]);
   return (
     <div className='mt-10'>
       <h1 className='text-xl font-semibold text-gray-900'>Products we are proud of</h1>
-      <div className='flex flex-row'>
-        sdafs
+      <div className='flex gap-5'>
+        {randomProduct.map((card) => (
+          <CardComponent key={card.key} cardKey={card.key} img={card.img} Title={card.Title} category={card.category} price={card.price} />
+        ))}
       </div>
     </div>
   )
 }
 
-export default ProudProducts
+export default ProudProducts;
